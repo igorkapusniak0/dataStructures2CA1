3d33cf27c5e0eed602a89c8d4f859a993272717b
igor kapusniak <20102236@gmail.com>
2024-02-08 21:43:42 +0000

    Day:3 Implemented non working union find changed colour detection to HSB from RGB

--- src/main/java/API/API.java
+++ src/main/java/API/API.java
@@ -11,55 +11,54 @@ import javafx.scene.paint.Color;
 import java.util.ArrayList;
 
 public class API {
-    public static Image detectColour(Image image, Slider redIntensitySlider, Slider greenIntensitySlider, Slider blueIntensitySlider){
-        if (image != null){
-            int height = (int) image.getHeight();
-            int width = (int) image.getWidth();
+    public static Image detectColour(Image image, Slider hueSlider, Slider saturationSlider, Slider brightnessSlider, double hueTolerance, double saturationTolerance, double brightnessTolerance) {
+        if (image == null) return null;
 
-            double redIntensity = redIntensitySlider.getValue();
-            double greenIntensity = greenIntensitySlider.getValue();
-            double blueIntensity = blueIntensitySlider.getValue();
+        int width = (int) image.getWidth();
+        int height = (int) image.getHeight();
+        WritableImage writableImage = new WritableImage(width, height);
+        PixelReader pixelReader = image.getPixelReader();
+        PixelWriter pixelWriter = writableImage.getPixelWriter();
 
-            WritableImage writableImage = new WritableImage(width,height);
-            PixelReader pixelReader = image.getPixelReader();
-            PixelWriter pixelWriter = writableImage.getPixelWriter();
+        double hueValue = hueSlider.getValue();
+        double saturationValue = saturationSlider.getValue();
+        double brightnessValue = brightnessSlider.getValue();
 
-            for (int x = 0; x<width; x++){
-                for (int y = 0; y<height; y++){
-                    Color colour = pixelReader.getColor(x,y);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                Color color = pixelReader.getColor(x, y);
 
+                boolean matchesHue = color.getHue() >= hueValue - hueTolerance && color.getHue() <= hueValue + hueTolerance;
+                boolean matchesSaturation = color.getSaturation() >= saturationValue - saturationTolerance && color.getSaturation() <= saturationValue + saturationTolerance;
+                boolean matchesBrightness = color.getBrightness() >= brightnessValue - brightnessTolerance && color.getBrightness() <= brightnessValue + brightnessTolerance;
 
-                    if (colour.getRed()>redIntensity && colour.getGreen()>greenIntensity && colour.getBlue()>blueIntensity){
-                        pixelWriter.setColor(x,y,Color.rgb(255,255,255));
-                    }else{
-                        pixelWriter.setColor(x,y,Color.rgb(0,0,0));
-                    }
+                if (matchesHue && matchesSaturation && matchesBrightness) {
+                    pixelWriter.setColor(x, y, Color.rgb(255, 255, 255));
+                } else {
+                    pixelWriter.setColor(x, y, Color.rgb(0, 0, 0));
                 }
             }
-            return writableImage;
         }
-        else {
-            return null;
-        }
+
+        return writableImage;
     }
 
 
-    public static int[] method(Image blackAndWhiteImage, int[] pixels){
+    public static int[] findWhite(Image blackAndWhiteImage){
         if (blackAndWhiteImage != null) {
             int height = (int) blackAndWhiteImage.getHeight();
             int width = (int) blackAndWhiteImage.getWidth();
             Color white = new Color(1,1,1,1);
+            int[] pixels = new int[height*width];
             PixelReader pixelReader = blackAndWhiteImage.getPixelReader();
 
-            for (int x = 0; x<width; x++){
-                for (int y = 0; y<height; y++){
+            for (int y = 0; y<width; y++){
+                for (int x = 0; x<height; x++){
                     Color colour = pixelReader.getColor(x,y);
                     if (colour.equals(white)){
-                        pixels[x * width + y] = x * width + y;
-                    }
-                    else{
-
-                        pixels[x * width + y] = -1;
+                        pixels[x+y] = x;
+                    }else{
+                        pixels[x+y] = -1;
                     }
                 }
             }
@@ -68,18 +67,26 @@ public class API {
             return null;
         }
     }
-    public static SetNode<?> find(SetNode<?> node) {
-        return node.parent==null ? node : (find(node.parent));
-    }
     public static int find(int[] a, int id) {
         return a[id] == id ? id : (a[id] = find(a, a[id]));
     }
-        public static void unionBySize(SetNode<?> p, SetNode<?> q) {
-        SetNode<?> rootp=find(p);
-        SetNode<?> rootq=find(q);
-        SetNode<?> biggerRoot=rootp.size>=rootq.size ? rootp : rootq;
-        SetNode<?> smallerRoot=biggerRoot==rootp ? rootq : rootp;
-        smallerRoot.parent=biggerRoot;
-        biggerRoot.size+=smallerRoot.size;
+    public static void unionByHeight(int[] a, int p, int q) {
+        int rootp=find(a,p);
+        int rootq=find(a,q);
+        int deeperRoot=a[rootp]<a[rootq] ? rootp : rootq;
+        int shallowerRoot=deeperRoot==rootp ? rootq : rootp;
+        int temp=a[shallowerRoot];
+        a[shallowerRoot]=deeperRoot;
+        if(a[deeperRoot]==temp) a[deeperRoot]--;
     }
+    public static void unionBySize(int[] a, int p, int q) {
+        int rootp=find(a,p);
+        int rootq=find(a,q);
+        int biggerRoot=a[rootp]<a[rootq] ? rootp : rootq;
+        int smallerRoot=biggerRoot==rootp ? rootq : rootp;
+        int smallSize=a[smallerRoot];
+        a[smallerRoot]=biggerRoot;
+        a[biggerRoot]+=smallSize;
+    }
+
 }
--- src/main/java/Controllers/MainController.java
+++ src/main/java/Controllers/MainController.java
@@ -13,9 +13,7 @@ public class MainController {
     private Image image = null;
     private Image blackAndWhite = null;
     @FXML
-    ImageView originalImageView = new ImageView();
-    @FXML
-    ImageView blackAndWhiteImageView = new ImageView();
+    ImageView imageView = new ImageView();
     @FXML
     Slider redIntensitySlider = new Slider();
     @FXML
@@ -23,18 +21,40 @@ public class MainController {
     @FXML
     Slider blueIntensitySlider = new Slider();
     @FXML
+    Slider hueIntensitySlider = new Slider();
+    @FXML
+    Slider saturationIntensitySlider = new Slider();
+    @FXML
+    Slider brightnessIntensitySlider = new Slider();
+
+    int hueTolerance = 50;
+    double saturationTolerance = .2;
+    double brightnessTolerance = .2;
+    @FXML
     public void initialize() {
         redIntensitySlider.valueChangingProperty().addListener((obs, oldVal, newVal) -> {
-            blackAndWhite = API.detectColour(image, redIntensitySlider,greenIntensitySlider,blueIntensitySlider);
-            blackAndWhiteImageView.setImage(blackAndWhite);
+            blackAndWhite = API.detectColour(image, redIntensitySlider,greenIntensitySlider,blueIntensitySlider,hueIntensitySlider.getValue(),saturationIntensitySlider.getValue(),brightnessIntensitySlider.getValue());
+            imageView.setImage(blackAndWhite);
         });
         greenIntensitySlider.valueChangingProperty().addListener((obs, oldVal, newVal) -> {
-            blackAndWhite = API.detectColour(image, redIntensitySlider,greenIntensitySlider,blueIntensitySlider);
-            blackAndWhiteImageView.setImage(blackAndWhite);
+            blackAndWhite = API.detectColour(image, redIntensitySlider,greenIntensitySlider,blueIntensitySlider,hueIntensitySlider.getValue(),saturationIntensitySlider.getValue(),brightnessIntensitySlider.getValue());
+            imageView.setImage(blackAndWhite);
         });
         blueIntensitySlider.valueChangingProperty().addListener((obs, oldVal, newVal) -> {
-            blackAndWhite = API.detectColour(image,redIntensitySlider,greenIntensitySlider,blueIntensitySlider);
-            blackAndWhiteImageView.setImage(blackAndWhite);
+            blackAndWhite = API.detectColour(image,redIntensitySlider,greenIntensitySlider,blueIntensitySlider,hueIntensitySlider.getValue(),saturationIntensitySlider.getValue(),brightnessIntensitySlider.getValue());
+            imageView.setImage(blackAndWhite);
+        });
+        hueIntensitySlider.valueChangingProperty().addListener((obs, oldVal, newVal) -> {
+            blackAndWhite = API.detectColour(image, redIntensitySlider,greenIntensitySlider,blueIntensitySlider,hueIntensitySlider.getValue(),saturationIntensitySlider.getValue(),brightnessIntensitySlider.getValue());
+            imageView.setImage(blackAndWhite);
+        });
+        saturationIntensitySlider.valueChangingProperty().addListener((obs, oldVal, newVal) -> {
+            blackAndWhite = API.detectColour(image, redIntensitySlider,greenIntensitySlider,blueIntensitySlider,hueIntensitySlider.getValue(),saturationIntensitySlider.getValue(),brightnessIntensitySlider.getValue());
+            imageView.setImage(blackAndWhite);
+        });
+        brightnessIntensitySlider.valueChangingProperty().addListener((obs, oldVal, newVal) -> {
+            blackAndWhite = API.detectColour(image,redIntensitySlider,greenIntensitySlider,blueIntensitySlider,hueIntensitySlider.getValue(),saturationIntensitySlider.getValue(),brightnessIntensitySlider.getValue());
+            imageView.setImage(blackAndWhite);
         });
 
 
@@ -43,7 +63,6 @@ public class MainController {
     @FXML
     protected void openImage() {
         FileChooser fileChooser = new FileChooser();
-        blackAndWhiteImageView.setOpacity(0);
         fileChooser.setTitle("Open Image");
         fileChooser.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg", "*.webp"),
@@ -53,20 +72,33 @@ public class MainController {
             image = new Image(selectedFile.toURI().toString(),500,500,false,false);
 
         }
-        originalImageView.setImage(image);
+        imageView.setImage(image);
     }
 
     @FXML
     protected void nextImage(){
-        originalImageView.setOpacity(0);
-        blackAndWhiteImageView.setOpacity(1);
-        blackAndWhite = API.detectColour(image, redIntensitySlider, greenIntensitySlider, blueIntensitySlider);
-        blackAndWhiteImageView.setImage(blackAndWhite);
+        blackAndWhite = API.detectColour(image, redIntensitySlider, greenIntensitySlider, blueIntensitySlider,hueIntensitySlider.getValue(),saturationIntensitySlider.getValue(),brightnessIntensitySlider.getValue());
+        imageView.setImage(blackAndWhite);
+        int[] pixels = new int[(int) image.getHeight()* (int) image.getWidth()];
+        pixels = API.findWhite(blackAndWhite);
+        unionFind(pixels);
     }
     @FXML
     protected void previousImage(){
-        originalImageView.setOpacity(1);
-        blackAndWhiteImageView.setOpacity(0);
+        imageView.setImage(image);
+    }
+
+    private void unionFind(int[] pixels){
+        for (int i = 0; i<pixels.length; i++){
+            int down = i+(int) blackAndWhite.getWidth();
+            int right = i+1;
+            if (pixels[i]!=-1 && pixels[right]!= -1 && right<pixels.length){
+                API.unionByHeight(pixels,i,right);
+            }
+            if (pixels[i] != -1 && pixels[down]!=-1 && down<=pixels.length-blackAndWhite.getHeight()){
+                API.unionByHeight(pixels,i,down);
+            }
+        }
     }
 
 
